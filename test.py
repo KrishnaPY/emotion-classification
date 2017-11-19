60439875a74c85e1d8055bb5a145adafbd6f4360
KrishnPY <krishnapraveen383@gmail.com>
2017-11-19 21:15:55 +0530

    normalized features in test.py

--- test.py
+++ test.py
@@ -1,12 +1,18 @@
 import matplotlib.pyplot as plt  
 import numpy as np   
 import csv
-
+from sklearn.preprocessing import StandardScaler
 
 cry = np.genfromtxt("adult_cry_extract40.csv", delimiter=',', skip_header=True)
 laugh = np.genfromtxt("adult_laugh_extract40.csv", delimiter=',', skip_header=True)
 
 #print cry[30,:], laugh[30,:]
+
+scaler = StandardScaler() # normalizing the data to unit variance
+cry[:,1:] = scaler.fit_transform(cry[:,1:])
+laugh[:,1:] = scaler.fit_transform(laugh[:,1:])
+print cry[5:7,:], laugh[5:7,:]
+
 cry_mfcc = cry[:,3:]
 laugh_mfcc = laugh[:,3:]
 
